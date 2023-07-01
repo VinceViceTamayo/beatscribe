7f53e25c1182e721c9d2f161d2363ff338cd6d96
VinceTamayo <vincevicetamayo@gmail.com>
2023-07-01 23:59:16 +0800

    add player UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Player from "./components/Player"
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Player />
     </>
   )
 }
---
+++ src/components/Player.js
@@ -0,0 +1,45 @@
+import { React, useState } from "react"
+import { ArrowPathRoundedSquareIcon, BackwardIcon, ForwardIcon, HeartIcon, PlayIcon, QueueListIcon } from '@heroicons/react/24/outline'
+
+export default function Player() {
+    const [playButton, setPlayButton] = useState(false);
+
+    const play = () => {
+        setPlayButton(true);
+        alert("Play button was clicked")
+    }
+
+    return (
+        <div class="container min-w-100px w-auto max-w-100 mx-auto mt-10">
+            <div class="card flex flex-col items-center bg-gradient-to-tr from-slate-300 to-slate-700 text-xl font-mono p-4 rounded-md text-white" >
+                <div class="cover flex flex-col items-center min-w-80px w-auto max-w-880px">
+                    <img src="https://w0.peakpx.com/wallpaper/574/25/HD-wallpaper-amoled-astronaut-929-abstract-dark-minimal-q-simple-space-theme.jpg" alt="Album Cover" class="w-3/6 rounded-xl" />
+                    <p class="-translate-y-10 w-3/6 text-center break-words">The Edge of Space</p>
+                </div>
+                {/* <audio id="song" class="block w-full max-w-md mx-auto" controls>
+                    <source src="https://open.spotify.com/track/7DE0I3buHcns00C0YEsYsY?si=5e0442c12f514f04" type="audio/mpeg" />
+                </audio> */}
+                <div class="flex flex-row">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <HeartIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <BackwardIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <PlayIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <ForwardIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <ArrowPathRoundedSquareIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                        <QueueListIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
