897585e4b553d0007ecfa25e18bb361618d857f5
VinceViceTamayo <vincevicetamayo@gmail.com>
2023-07-03 00:48:14 +0800

    add event listener template

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,11 +2,29 @@ import { React, useState } from "react"
 import { ArrowPathRoundedSquareIcon, BackwardIcon, ForwardIcon, HeartIcon, PlayIcon, QueueListIcon } from '@heroicons/react/24/outline'
 
 export default function Player() {
-    const [playButton, setPlayButton] = useState(false);
+    // const [playButton, setPlayButton] = useState(false);
+    const favorite = () => {
+        alert("Favorite button was clicked")
+    }
+
+    const backward = () => {
+        alert("Backward button was clicked")
+    }
 
     const play = () => {
-        setPlayButton(true);
         alert("Play button was clicked")
+    }
+
+    const forward = () => {
+        alert("Forward button was clicked")
+    }
+
+    const repeat = () => {
+        alert("Repeat button was clicked")
+    }
+
+    const list = () => {
+        alert("List button was clicked")
     }
 
     return (
@@ -20,22 +38,22 @@ export default function Player() {
                     <source src="https://open.spotify.com/track/7DE0I3buHcns00C0YEsYsY?si=5e0442c12f514f04" type="audio/mpeg" />
                 </audio> */}
                 <div class="flex flex-row">
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={favorite}>
                         <HeartIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={backward}>
                         <BackwardIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={play}>
                         <PlayIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={forward}>
                         <ForwardIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={repeat}>
                         <ArrowPathRoundedSquareIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
-                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer">
+                    <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={list}>
                         <QueueListIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
                 </div>
