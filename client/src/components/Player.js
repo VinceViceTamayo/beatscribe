34767e71e68c6027d71ef24d6dbfca20cc0d9e13
VinceTamayo <vincevicetamayo@gmail.com>
2023-07-11 23:09:24 +0800

    additional functionalities

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react"
+import { React, useState, useEffect, useRef } from "react"
 import payload from "../../package.json"
 import { ArrowPathRoundedSquareIcon, BackwardIcon, ForwardIcon, HeartIcon, PlayIcon, PauseIcon, QueueListIcon } from '@heroicons/react/24/outline'
 
@@ -6,6 +6,10 @@ export default function Player() {
     const [IconHolder, setIconHolder] = useState(PlayIcon)
     const [playButton, setPlayButton] = useState(1);
     const [currentSong, setCurrentSong] = useState("");
+    const [title, setTitle] = useState("");
+    const [artist, setArtist] = useState("");
+    const [lyrics, setLyrics] = useState("");
+    const mainPlayer = useRef();
 
     useEffect(() => {
         fetch(`${payload.proxy}/song`)
@@ -14,20 +18,22 @@ export default function Player() {
         })
         .then(data => {
             const {artist, lyrics, title, track} = data
-            setCurrentSong(track)
-            console.log(currentSong)
+            setCurrentSong(track.substring(1));
+            setTitle(title);
+            setArtist(artist);
+            setLyrics(lyrics)
+            mainPlayer.current.load();
         })
-    }, [currentSong])
+    }, [])
 
     useEffect(()=>{
         if(playButton%2 === 0){
             setIconHolder(PauseIcon)
-            //console.log(payload)
-
-            //Send play post request to API
+            mainPlayer.current.play();
         }else{
             // Send pause post request to API
-            setIconHolder(PlayIcon)
+            setIconHolder(PlayIcon);
+            mainPlayer.current.pause();
         }
     },[playButton])
 
@@ -56,16 +62,16 @@ export default function Player() {
     }
 
     return (
-        <div class="container min-w-100px w-auto max-w-100 mx-auto mt-10">
-            <div class="card flex flex-col items-center bg-gradient-to-tr from-slate-300 to-slate-700 text-xl font-mono p-4 rounded-md text-white" >
-                <div class="cover flex flex-col items-center min-w-80px w-auto max-w-880px">
-                    <img src="https://w0.peakpx.com/wallpaper/574/25/HD-wallpaper-amoled-astronaut-929-abstract-dark-minimal-q-simple-space-theme.jpg" alt="Album Cover" class="w-3/6 rounded-xl" />
-                    <p class="-translate-y-10 w-3/6 text-center break-words">The Edge of Space</p>
+        <div className="container min-w-100px w-auto max-w-100 mx-auto mt-10">
+            <div className="card flex flex-col items-center bg-gradient-to-tr from-slate-300 to-slate-700 text-xl font-mono p-4 rounded-md text-white" >
+                <div className="cover flex flex-col items-center min-w-80px w-auto max-w-880px">
+                    <img src="https://w0.peakpx.com/wallpaper/574/25/HD-wallpaper-amoled-astronaut-929-abstract-dark-minimal-q-simple-space-theme.jpg" alt="Album Cover" className="w-3/6 rounded-xl" />
+                    <p className="-translate-y-10 w-3/6 text-center break-words">{`${title} by ${artist}`}</p>
                 </div>
-                <audio id="song" class="block w-full max-w-md mx-auto" controls>
-                    <source src={currentSong} type="audio/mpeg" />
+                <audio id="song" className="block w-full max-w-md mx-auto mb-4" controls ref={mainPlayer}>
+                    <source src={`${payload.proxy}${currentSong}`} type="audio/mpeg" />
                 </audio>
-                <div class="flex flex-row">
+                <div className="flex flex-row">
                     <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={favorite}>
                         <HeartIcon className="h-6 w-6 text-white" aria-hidden="true" />
                     </div>
@@ -83,6 +89,16 @@ export default function Player() {
                     </div>
                     <div className="left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-slate-950 mx-1 cursor-pointer" onClick={list}>
                         <QueueListIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </div>
+                    <button data-popover-target="popover-click" data-popover-placement="top" data-popover-trigger="click" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lyrics</button>
+                    <div data-popover id="popover-click" role="tooltip" className="absolute z-10 invisible inline-block w-1/4 h-100 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+                        <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                            <h3 className="font-semibold text-gray-900 dark:text-white">Lyrics provided by Blackbox AI</h3>
+                        </div>
+                        <div className="px-3 py-2 whitespace-pre-line">
+                            <p>{lyrics}</p>
+                        </div>
+                        <div data-popper-arrow></div>
                     </div>
                 </div>
             </div>
